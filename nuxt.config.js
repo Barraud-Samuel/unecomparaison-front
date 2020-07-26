e8f1ea011c505d903c3e9206f113143b3c3639a9
barraud-samuel <samuel.barraud@hetic.net>
2020-07-26 22:10:52 +0200

    add infinite loading

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'http://www.une-comparaison.space/',
   },
   /*
   ** Build configuration
